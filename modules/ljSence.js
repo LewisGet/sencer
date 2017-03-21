94618e8b8f5beb6592ba3a4da9fde3875eab5cb2
Lewis Jang <lewis.aedes@gmail.com>
2017-03-21 21:16:36 +0800

    rebuild project

--- modules/ljSence.js
+++ modules/ljSence.js
@@ -1,14 +1,7 @@
-exports.sence = function(basicEntity) {
-    this.world = basicEntity.world;
-    this.entity = basicEntity;
-
-    this.locationToArray = function (value) {
-        return [value.x, value.y, value.z];
-    };
-
-    this.createLocation = function (value) {
-        return new org.bukkit.Location(this.world, value[0], value[1], value[2]);
-    };
+exports.sence = function() {
+    this.nj = require("numjs");
+    this.lo = require("lodash");
+    this.world = (server.getWorlds())[0];
 
     this.getKevinEntity = function () {
         return org.bukkit.Bukkit.getPlayer("KevinWus");
@@ -18,83 +11,58 @@ exports.sence = function(basicEntity) {
         return org.bukkit.Bukkit.getPlayer("LewisJang");
     };
 
-    this.lineXExecute = function (start, level, diff, execute_function) {
-        var x = location.x;
-        var y = location.y;
-        var z = location.z;
-
-        for (var execute_index = 0; execute_index < level; execute_index += diff)
+    this.getLocation = function (value) {
+        if (value.tolist)
         {
-            var execute_location = [
-                x + execute_index,
-                y,
-                z
-            ];
-
-            execute_location = this.createLocation(execute_location);
-
-            execute_function(execute_location);
+            value = value.tolist();
         }
+
+        if (Array.isArray(value))
+        {
+            return new org.bukkit.Location(this.world, value[0], value[1], value[2]);
+        }
+
+        return new org.bukkit.Location(this.world, value.x, value.y, value.z);
     };
 
-    this.lineZExecute = function (start, level, diff, execute_function) {
-        var x = location.x;
-        var y = location.y;
-        var z = location.z;
+    this.execute = function (location, execute_function) {
+        location = this.getLocation(location);
 
-        for (var execute_index = 0; execute_index < level; execute_index += diff)
-        {
-            var execute_location = [
-                x,
-                y,
-                z + execute_index
-            ];
-
-            execute_location = this.createLocation(execute_location);
-
-            execute_function(execute_location);
-        }
+        execute_function(location);
     };
 
-    this.area2DExecute = function (location, level, diff_x, diff_z, execute_function) {
-        var x = location.x;
-        var y = location.y;
-        var z = location.z;
-
-        for (var execute_x_index = 0; execute_x_index < level; execute_x_index += diff_x)
-        {
-            for (var execute_z_index = 0; execute_z_index < level; execute_z_index += diff_z)
-            {
-                var execute_location = [
-                    x + execute_x_index,
-                    y,
-                    z + execute_z_index
-                ];
-
-                execute_location = this.createLocation(execute_location);
-
-                execute_function(execute_location);
-            }
-        }
+    this.each_execute = function (locations, execute_function) {
+        locations.forEach(function(location, index) {
+            execute_function(location, index);
+        });
     };
 
-    this.area3DExecute = function (location, level, diff_x, diff_y, diff_z, execute_function) {
-        var x = location.x;
-        var y = location.y;
-        var z = location.z;
+    this.rangeXYZ = function (basic, x, y, z) {
+        var locations = [];
 
-        for (var execute_y_index = 0; execute_y_index < level; execute_y_index += diff_x)
-        {
-            var execute_location = [
-                x + execute_x_index,
-                y,
-                z + execute_z_index
-            ];
+        nj_basic = this.nj.array(basic);
 
-            execute_location = this.createLocation(execute_location);
+        x = x.split("~");
+        y = y.split("~");
+        z = z.split("~");
 
-            this.area2DExecute(execute_location, level, diff_x, diff_z, execute_function);
-        }
+        x = this.lo.range(parseInt(x[0]), parseInt(x[1]));
+        y = this.lo.range(parseInt(y[0]), parseInt(y[1]));
+        z = this.lo.range(parseInt(z[0]), parseInt(z[1]));
+
+        if (x.length == 0) { x = [basic[0]]; }
+        if (y.length == 0) { y = [basic[1]]; }
+        if (z.length == 0) { z = [basic[2]]; }
+
+        x.forEach(function(x_value) {
+            y.forEach(function(y_value) {
+                z.forEach(function(z_value) {
+                    locations.push((nj_basic.add([x_value, y_value, z_value])));
+                });
+            });
+        });
+
+        return locations;
     };
 };
 
@@ -115,5 +83,22 @@ exports.explosion = function (sence) {
 
     this.createLewis = function () {
         this.createBasicExplosion(this.sence.getLewisEntity().location);
+    };
+};
+
+exports.block = function (sence) {
+    this.sence = sence;
+    this.set = function (location, typeid, data) {
+        if (Array.isArray(location))
+        {
+            location = this.sence.createLocation(location);
+        }
+
+        location.block.setTypeId(typeid);
+        location.block.setData(data);
+    };
+
+    this.setBasic = function (location, typeid) {
+        this.set(location, typeid, 0);
     };
 };
---
+++ plugins/ljMain.js
@@ -0,0 +1,2 @@
+exports._ = require("ljSence");
+exports.sence = new exports._.sence();
