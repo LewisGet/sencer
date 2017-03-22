0295930bd3e4a97602f709a91b16d9726c57ec3d
Lewis Jang <lewis.aedes@gmail.com>
2017-03-22 15:28:03 +0800

    add some functions

--- modules/ljSence.js
+++ modules/ljSence.js
@@ -70,6 +70,8 @@ exports.explosion = function (sence) {
     this.sence = sence;
 
     this.create = function (location, level) {
+        location = this.sence.getLocation(location);
+
         this.sence.world.createExplosion(location, level);
     };
 
@@ -88,17 +90,140 @@ exports.explosion = function (sence) {
 
 exports.block = function (sence) {
     this.sence = sence;
+
     this.set = function (location, typeid, data) {
-        if (Array.isArray(location))
-        {
-            location = this.sence.createLocation(location);
-        }
+        location = this.sence.getLocation(location);
 
         location.block.setTypeId(typeid);
         location.block.setData(data);
     };
 
+    this.remove = function (location) {
+        this.set(location, 0, 0);
+    };
+
+    this.removeList = function (locations) {
+        var block = this;
+
+        locations.forEach(function(location) {
+            block.remove(location);
+        });
+    };
+
+    this.setList = function (locations, typeid, data) {
+        var block = this;
+
+        locations.forEach(function(location) {
+            block.set(location, typeid, data);
+        });
+    };
+
     this.setBasic = function (location, typeid) {
         this.set(location, typeid, 0);
+    };
+};
+
+exports.entity = function (sence) {
+    this.sence = sence;
+    this.type = org.bukkit.entity.EntityType;
+
+    this.create = function (location, type) {
+        location = this.sence.getLocation(location);
+
+        return this.sence.world.spawnEntity(location, type);
+    };
+
+    this.createList = function (locations, type) {
+        var entity = this;
+
+        locations.forEach(function(location) {
+            entity.createList(location, type);
+        });
+    };
+};
+
+exports.lightning = function (sence) {
+    this.sence = sence;
+
+    this.create = function (location) {
+        location = this.sence.getLocation(location);
+
+        this.sence.world.strikeLightning(location);
+    };
+
+    this.createList = function (locations) {
+        var lightning = this;
+
+        locations.forEach(function(location) {
+            lightning.createList(location);
+        });
+    };
+};
+
+exports.resource = function (sence) {
+    this.sence = sence;
+    this.type = org.bukkit.Material;
+
+    this.create = function (location, type) {
+        location = this.sence.getLocation(location);
+        var item = new org.bukkit.inventory.ItemStack(type, 1);
+        
+        this.sence.world.dropItem(location, item);
+    };
+
+    this.createList = function (locations, type) {
+        var resource = this;
+
+        locations.forEach(function(location) {
+            resource.createList(location, type);
+        });
+    };
+};
+
+exports.fly = function (sence) {
+    this.sence = sence;
+    this.entity;
+    this.x = 0;
+    this.y = 5;
+    this.z = 0;
+
+    this.fly_vec = function () {
+        return new org.bukkit.util.Vector(this.x, this.y, this.z);
+    };
+
+    this.fly_entity = function () {
+        this.entity.setVelocity(this.fly_vec());
+    };
+
+    this.fly_lewis = function () {
+        this.entity = this.sence.getLewisEntity();
+
+        this.fly_entity();
+    };
+
+    this.fly_kevin = function () {
+        this.entity = this.sence.getKevinEntity();
+
+        this.fly_entity();
+    };
+
+    this.fly_block = function (location, typeid, data) {
+        location = this.sence.getLocation(location);
+
+        this.entity = this.sence.world.spawnFallingBlock(location, typeid, data);
+
+        this.fly_entity();
+    };
+
+    this.fly_block_with_lewis = function (location, typeid, data) {
+        this.fly_block(location, typeid, data);
+
+        this.entity.setPassenger(this.sence.getLewisEntity());
+    };
+
+    this.fly_block_with_kevin = function (location, typeid, data) {
+        this.fly_block(location, typeid, data);
+
+        this.entity.setPassenger(this.sence.getKevinEntity());
     };
 };
