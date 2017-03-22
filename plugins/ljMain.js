5bca8f0bff03fb3be83ffa9a1d192e51b62d339a
Lewis Jang <lewis.aedes@gmail.com>
2017-03-22 21:23:12 +0800

    create all the function in plugins

--- plugins/ljMain.js
+++ plugins/ljMain.js
@@ -1,2 +1,10 @@
 exports._ = require("ljSence");
-exports.sence = new exports._.sence();
+
+exports.sence     = new exports._.sence();
+exports.explosion = new exports._.explosion(exports.sence);
+exports.block     = new exports._.block(exports.sence);
+exports.entity    = new exports._.entity(exports.sence);
+exports.lightning = new exports._.lightning(exports.sence);
+exports.resource  = new exports._.resource(exports.sence);
+exports.fly       = new exports._.fly(exports.sence);
+exports.hurt      = new exports._.hurt(exports.sence);
