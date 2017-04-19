b40655f9f0bb8803cae6f4f7659cb5283513569d
Lewis Jang <lewis.aedes@gmail.com>
2017-04-19 20:35:55 +0800

    add explosion action

--- plugins/ljMain.js
+++ plugins/ljMain.js
@@ -8,3 +8,31 @@ exports.lightning = new exports._.lightning(exports.sence);
 exports.resource  = new exports._.resource(exports.sence);
 exports.fly       = new exports._.fly(exports.sence);
 exports.hurt      = new exports._.hurt(exports.sence);
+
+
+var explosion_action = function (event) {
+    var blocks = event.blockList();
+
+    for (var i = 0; i < blocks.length; i++)
+    {
+        var block = blocks[i];
+
+        var x = (Math.random() - 0.5) * 10;
+        var y = (Math.random() * 5);
+        var z = (Math.random() - 0.5) * 10;
+
+        var fly_block = block.world.spawnFallingBlock(block.location, block.typeId, block.data);
+
+        fly_block.setVelocity(new org.bukkit.util.Vector(x, y, z));
+
+        block.setTypeId(0);
+    }
+};
+
+events.blockExplode(function (event) {
+    explosion_action(event);
+});
+
+events.entityExplode(function (event) {
+    explosion_action(event);
+});
