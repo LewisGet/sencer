a538363f65e500cbf45871523621a6bd10ece810
Lewis Jang <lewis.aedes@gmail.com>
2019-02-11 17:12:51 +0800

    add string includes function

--- modules/ljSence.js
+++ modules/ljSence.js
@@ -8,6 +8,22 @@ if (typeof Array.prototype.forEach != 'function') {
     };
 }
 
+if (!String.prototype.includes) {
+    Object.defineProperty(String.prototype, 'includes', {
+        value: function(search, start) {
+            if (typeof start !== 'number') {
+                start = 0
+            }
+
+            if (start + search.length > this.length) {
+                return false
+            } else {
+                return this.indexOf(search, start) !== -1
+            }
+        }
+    })
+}
+
 exports.sence = function() {
     this.world = (server.getWorlds())[0];
 
